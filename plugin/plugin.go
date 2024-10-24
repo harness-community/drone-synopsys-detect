a36b81dbbcbcd3b4de6087e24e23c5c3d78a23b7
Anshika Anand <anshika.anand@harness.io>
2024-10-24 18:44:12 +0530

    Migrating files to harness-community

---
+++ plugin/plugin.go
@@ -0,0 +1,105 @@
+// Copyright 2024 the Drone Authors. All rights reserved.
+// Use of this source code is governed by the Blue Oak Model License
+// that can be found in the LICENSE file.
+
+package plugin
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+)
+
+type Plugin struct {
+	BlackduckURL            string `envconfig:"PLUGIN_BLACKDUCK_URL"`
+	BlackduckToken          string `envconfig:"PLUGIN_BLACKDUCK_TOKEN"`
+	BlackduckProject        string `envconfig:"PLUGIN_BLACKDUCK_PROJECT"`
+	BlackduckOfflineMode    bool   `envconfig:"PLUGIN_BLACKDUCK_OFFLINEMODE"`
+	BlackduckTestConnection bool   `envconfig:"PLUGIN_BLACKDUCK_TEST_CONNECTION"`
+	BlackduckOfflineBDIO    bool   `envconfig:"PLUGIN_BLACKDUCK_OFFLINE_BDIO"`
+	BlackduckTrustCerts     bool   `envconfig:"PLUGIN_BLACKDUCK_TRUST_CERTS"`
+	BlackduckTimeout        int    `envconfig:"PLUGIN_BLACKDUCK_TIMEOUT"`
+	BlackduckScanMode       string `envconfig:"PLUGIN_BLACKDUCK_SCAN_MODE"`
+	BLackduckProperties     string `envconfig:"PLUGIN_BLACKDUCK_PROPERTIES"`
+	LogLevel                string `envconfig:"PLUGIN_LOG_LEVEL"`
+}
+
+func (p *Plugin) Exec(ctx context.Context) error {
+	if err := runBlackDuckScan(p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runBlackDuckScan(p *Plugin) error {
+	bdURL := p.BlackduckURL
+	bdToken := p.BlackduckToken
+	bdProject := p.BlackduckProject
+
+	if bdURL == "" || bdToken == "" || bdProject == "" {
+		return fmt.Errorf("BLACKDUCK_URL, BLACKDUCK_TOKEN and BLACKDUCK_PROJECT environment variables must be set")
+	}
+
+	var command string
+	if runtime.GOOS == "linux" {
+		command = fmt.Sprintf("java -jar /opt/jar/synopsys-detect-9.7.0.jar --blackduck.url=\"%s\" --blackduck.api.token=\"%s\" --detect.project.name=\"%s\"", bdURL, bdToken, bdProject)
+	} else if runtime.GOOS == "windows" {
+		command = fmt.Sprintf("C:\\opt\\jar\\synopsys-detect-9.7.0.jar --blackduck.url=\"%s\" --blackduck.api.token=\"%s\" --detect.project.name=\"%s\"", bdURL, bdToken, bdProject)
+	}
+
+	if p.BlackduckOfflineMode {
+		command += " --blackduck.offline.mode=" + strconv.FormatBool(p.BlackduckOfflineMode)
+	}
+
+	if p.BlackduckTestConnection {
+		command += " --detect.test.connection=" + strconv.FormatBool(p.BlackduckTestConnection)
+	}
+
+	if p.BlackduckOfflineBDIO {
+		command += " --blackduck.offline.mode.force.bdio=" + strconv.FormatBool(p.BlackduckOfflineBDIO)
+	}
+
+	if p.BlackduckTrustCerts {
+		command += " --blackduck.trust.cert=" + strconv.FormatBool(p.BlackduckTrustCerts)
+	}
+
+	if p.BlackduckTimeout > 0 {
+		command += " --detect.timeout=" + strconv.Itoa(p.BlackduckTimeout)
+	}
+
+	// RAPID,STATELESS,INTELLIGENT
+	if p.BlackduckScanMode != "" {
+		switch p.BlackduckScanMode {
+		case "RAPID", "STATELESS", "INTELLIGENT":
+			command += " --detect.blackduck.scan.mode=" + p.BlackduckScanMode
+		default:
+			log.Printf("Unexpected BlackduckScanMode: %s \n Scan mode can be RAPID, STATELESS, INTELLIGENT.", p.BlackduckScanMode)
+		}
+	}
+
+	if p.BLackduckProperties != "" {
+		command += " " + p.BLackduckProperties
+	}
+
+	var cmd *exec.Cmd
+	if runtime.GOARCH == "amd64" || runtime.GOARCH == "arm64" {
+		cmd = exec.Command("bash", "-c", command)
+	} else {
+		cmd = exec.Command("cmd", "/C", command)
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Running command: %s\n", command)
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to execute Black Duck scan command: %w", err)
+	}
+
+	return nil
+}
---
+++ plugin/plugin_test.go
@@ -0,0 +1,207 @@
+package plugin
+
+import (
+	"context"
+	"os"
+	"runtime"
+	"testing"
+)
+
+func TestPlugin_Exec(t *testing.T) {
+	tests := []struct {
+		name    string
+		plugin  Plugin
+		wantErr bool
+	}{
+		{
+			name: "missing required fields",
+			plugin: Plugin{
+				BlackduckURL:     "",
+				BlackduckToken:   "",
+				BlackduckProject: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "basic configuration",
+			plugin: Plugin{
+				BlackduckURL:     "https://blackduck.example.com",
+				BlackduckToken:   "test-token",
+				BlackduckProject: "test-project",
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid scan mode",
+			plugin: Plugin{
+				BlackduckURL:      "https://blackduck.example.com",
+				BlackduckToken:    "test-token",
+				BlackduckProject:  "test-project",
+				BlackduckScanMode: "INVALID",
+			},
+			wantErr: false,
+		},
+	}
+
+	oldStdout := os.Stdout
+	oldStderr := os.Stderr
+	defer func() {
+		os.Stdout = oldStdout
+		os.Stderr = oldStderr
+	}()
+
+	devNull, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer devNull.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Stdout = devNull
+			os.Stderr = devNull
+
+			err := tt.plugin.Exec(context.Background())
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Plugin.Exec() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRunBlackDuckScan_CommandConstruction(t *testing.T) {
+	tests := []struct {
+		name       string
+		plugin     Plugin
+		wantSubstr []string
+		notWant    []string
+	}{
+		{
+			name: "basic command",
+			plugin: Plugin{
+				BlackduckURL:     "https://blackduck.example.com",
+				BlackduckToken:   "test-token",
+				BlackduckProject: "test-project",
+			},
+			wantSubstr: []string{
+				"--blackduck.url=\"https://blackduck.example.com\"",
+				"--blackduck.api.token=\"test-token\"",
+				"--detect.project.name=\"test-project\"",
+			},
+		},
+		{
+			name: "offline mode",
+			plugin: Plugin{
+				BlackduckURL:         "https://blackduck.example.com",
+				BlackduckToken:       "test-token",
+				BlackduckProject:     "test-project",
+				BlackduckOfflineMode: true,
+			},
+			wantSubstr: []string{
+				"--blackduck.offline.mode=true",
+			},
+		},
+		{
+			name: "test connection",
+			plugin: Plugin{
+				BlackduckURL:            "https://blackduck.example.com",
+				BlackduckToken:          "test-token",
+				BlackduckProject:        "test-project",
+				BlackduckTestConnection: true,
+			},
+			wantSubstr: []string{
+				"--detect.test.connection=true",
+			},
+		},
+		{
+			name: "scan mode RAPID",
+			plugin: Plugin{
+				BlackduckURL:      "https://blackduck.example.com",
+				BlackduckToken:    "test-token",
+				BlackduckProject:  "test-project",
+				BlackduckScanMode: "RAPID",
+			},
+			wantSubstr: []string{
+				"--detect.blackduck.scan.mode=RAPID",
+			},
+		},
+		{
+			name: "invalid scan mode",
+			plugin: Plugin{
+				BlackduckURL:      "https://blackduck.example.com",
+				BlackduckToken:    "test-token",
+				BlackduckProject:  "test-project",
+				BlackduckScanMode: "INVALID",
+			},
+			notWant: []string{
+				"--detect.blackduck.scan.mode=INVALID",
+			},
+		},
+		{
+			name: "timeout",
+			plugin: Plugin{
+				BlackduckURL:     "https://blackduck.example.com",
+				BlackduckToken:   "test-token",
+				BlackduckProject: "test-project",
+				BlackduckTimeout: 300,
+			},
+			wantSubstr: []string{
+				"--detect.timeout=300",
+			},
+		},
+		{
+			name: "additional properties",
+			plugin: Plugin{
+				BlackduckURL:        "https://blackduck.example.com",
+				BlackduckToken:      "test-token",
+				BlackduckProject:    "test-project",
+				BLackduckProperties: "--detect.tools=DETECTOR",
+			},
+			wantSubstr: []string{
+				"--detect.tools=DETECTOR",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			oldStdout := os.Stdout
+			oldStderr := os.Stderr
+			defer func() {
+				os.Stdout = oldStdout
+				os.Stderr = oldStderr
+			}()
+
+			devNull, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0666)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer devNull.Close()
+
+			os.Stdout = devNull
+			os.Stderr = devNull
+
+			err = runBlackDuckScan(&tt.plugin)
+			if err == nil {
+				for _, substr := range tt.wantSubstr {
+					if runtime.GOOS == "windows" {
+						if !containsString(tt.plugin.BlackduckURL, substr) {
+							t.Errorf("Command should contain %s", substr)
+						}
+					}
+				}
+				for _, substr := range tt.notWant {
+					if runtime.GOOS == "windows" {
+						if containsString(tt.plugin.BlackduckURL, substr) {
+							t.Errorf("Command should not contain %s", substr)
+						}
+					}
+				}
+			}
+		})
+	}
+}
+
+func containsString(s, substr string) bool {
+	return s != "" && substr != "" && s != substr
+}
